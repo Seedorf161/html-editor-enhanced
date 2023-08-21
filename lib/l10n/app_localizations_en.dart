824c69076cbbe73a08b5a7ecbdbe665a645edafd
Saheed Abdulrasaq <abdulrasaqlekan@gmail.com>
2023-08-21 11:03:50 +0100

    added generated files

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,21 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class SEn extends S {
+  SEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get insertImage => 'Insert Image';
+
+  @override
+  String get selectFromFiles => 'Select from files';
+
+  @override
+  String get chooseImage => 'Choose image';
+
+  @override
+  String get url => 'URL';
+
+  @override
+  String get cancel => 'Cancel';
+}
---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,21 @@
+import 'app_localizations.dart';
+
+/// The translations for Portuguese (`pt`).
+class SPt extends S {
+  SPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get insertImage => 'Inserir Imagem';
+
+  @override
+  String get selectFromFiles => 'Selecione da galeria';
+
+  @override
+  String get chooseImage => 'Escolher Imagem';
+
+  @override
+  String get url => 'URL';
+
+  @override
+  String get cancel => 'Cancelar';
+}
